2a82cebc1717a759467a7644ed4a3283f235d17d
ChewyToast <moll.bruno.99@gmail.com>
2022-10-07 22:24:10 +0200

    gnl

--- src/get_next_line_bonus.h
+++ src/get_next_line_bonus.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmoll-pe <bmoll-pe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 11:47:32 by bmoll-pe          #+#    #+#             */
-/*   Updated: 2022/06/15 11:47:33 by bmoll-pe         ###   ########.fr       */
+/*   Updated: 2022/10/07 22:23:45 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <stdlib.h>
 
 // LIBRARY FOR OPEN_MAX MACROS
-# include <limits.h>
+//# include <limits.h>
 
 char	*get_next_line(const int fd);
 
@@ -35,5 +35,6 @@ size_t	ft_strlenne(const char *s);
 char	*ft_substrr(char *total_buff, size_t start, size_t len);
 
 # define BUFFER_SIZE 1
+# define OPEN_MAX 1023
 
 #endif
